6d3c74cd98b5d81c850c9a297077936848b0345e
Myric September <myric.september@gmail.com>
2021-08-01 16:24:01 +0200

    add drawable icons

---
+++ app/src/main/java/com/myricseptember/memoryhunter/MemoryHunterApplication.kt
@@ -0,0 +1,8 @@
+package com.myricseptember.memoryhunter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MemoryHunterApplication : Application() {
+}
